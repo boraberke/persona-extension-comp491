a3725f1cf9c72d507bdd7b633d53716c90decdbc
beyzanurcoban <bcoban17@ku.edu.tr>
2022-12-15 16:47:54 +0300

    specialized search by using tagged data

--- web-crawling/WebCrawler.py
+++ web-crawling/WebCrawler.py
@@ -8,7 +8,29 @@ import threading
 import json
 from DoubleThreadMultitab import doubleThreadMultitab
 
-# queries = ["python", "stackoverflow", "github"]
+# list of tuples
+out_ex = [('Ufkun Ozalp', 'PERSON'), ('22 year old', 'DATE'), ('Turkish', 'NORP'), ('New York City', 'GPE'), ('Highway to Hell', 'WORK_OF_ART'), ('Ford', 'ORG'), ('Mustang GT 500', 'PRODUCT')]
+queries = []
+location = ''
+
+for tuple in out_ex:
+    if tuple[1] == 'GPE':
+        location = tuple[0]
+        queries.append(location + ' rental estate')
+        queries.append(location + ' places to go')
+    elif tuple[1] == 'WORK_OF_ART':
+        if location != '':
+            queries.append(tuple[0] + ' events in ' + location)
+        else:
+            queries.append(tuple[0] + ' events')
+    elif tuple[1] == 'PRODUCT':
+        queries.append(tuple[0] + ' prices')
+    elif tuple[1] == 'NORP':
+        queries.append(tuple[0] + ' food')
+
+print(queries)
+
+# queries = ["python", "stackoverflow", "github"] is a string array
 def findURLsByKeyword(queries):
     # https://docs.python-requests.org/en/master/user/quickstart/#custom-headers
     headers = {
@@ -32,9 +54,7 @@ def findURLsByKeyword(queries):
         
         for link in soup.select(".yuRUbf a"):
             resulted_link = link.get('href')
-            
-            if query in resulted_link.lower():
-                links.append(resulted_link)
+            links.append(resulted_link)
 
         results.append({
             "query": query,
@@ -58,9 +78,7 @@ def visitURLs(urlListJson):
 
     doubleThreadMultitab(allURLs, driver_path)
 
-
-
-
-
+#queries = ["New York City", "Highway to Hell", "Mustang GT 500"]
+findURLsByKeyword(queries)
 #queries = ["python", "stackoverflow", "github"]
 #visitURLs(findURLsByKeyword(queries))
